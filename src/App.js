ac725f44c8c7db4435d19344d13c8d9ba16dd53f
sunil patel <sunilbhu288@gmail.com>
2023-07-27 11:10:49 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar'
+import Home from "./components/Home";
+// import {Navbar} from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
